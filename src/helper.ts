faa3617c4546e36d960d70fc7d600408a1475f10
rocco <rocco.mormont@gmail.com>
2018-12-30 00:12:18 +0800

    remove babel plugins, fix es6 syntax

--- src/helper.ts
+++ src/helper.ts
@@ -2,8 +2,12 @@ interface CommonObject {
   [propName: string]: any;
 }
 
+function entries(source: CommonObject) {
+  return Object.keys(source).map((k) => [k, source[k]]);
+}
+
 export function shadowMerge(target: CommonObject, source: CommonObject): CommonObject {
-  return Object.entries(source).reduce((acc, [k, v]) => {
+  return entries(source).reduce((acc, [k, v]) => {
     acc[k] = v;
     return acc;
   }, target);
